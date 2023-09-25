60a3e7621ab3ec2dbe1700c2c57ec583a332d104
shahriar <mshahriar40@gmail.com>
2023-09-25 22:48:18 +0600

    viewBag, viewData implemented. Navbar merged with shared navbar. And footer changed.

---
+++ Lab1_Refined/Lab1/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+        public ActionResult Bio()
+        {
+            ViewBag.projectInBio = "xyz, jhu, lko";
+            ViewData["skills"] = "mn,op,bv,ng";
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/Model_View_Bind/Model_View_Bind/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Model_View_Bind.MvcApplication" Language="C#" %>
