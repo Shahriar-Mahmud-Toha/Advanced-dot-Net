d88649fe46ba3f4af7ebb13966af7204d5a1b2ea
shahriar <mshahriar40@gmail.com>
2023-09-28 20:57:02 +0600

    Week 2 Lab work: Form submission in same page task uploaded.

---
+++ Lab2/Lab2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            ViewBag.name = form["name"];
+            ViewBag.id = form["id"];
+            ViewBag.gender = form["gender"];
+            ViewBag.profession = form["profession"];
+            ViewBag.hobbies = form["hobbies"];
+            ViewBag.status = true;
+            TempData["success_msg"] = true;
+            return View();
+        }
+    }
+}
